3ce192c6f2a08c1685efd6c8d964f7701f2c79c7
ca-rampelotti <138331782+ca-rampelotti@users.noreply.github.com>
2025-11-20 15:51:18 -0300

    Feat: CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title','').strip()
+        content = request.POST.get('content','')
+        post = Post.objects.create(title=title, content=content, created_at=timezone.now())
+        return redirect('blog:post_detail', pk=post.pk)
+    return render(request, 'blog/post_form.html', {'post': None})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST.get('title', post.title)
+        post.content = request.POST.get('content', post.content)
+        post.save()
+        return redirect('blog:post_detail', pk=post.pk)
+    return render(request, 'blog/post_form.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog:post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
