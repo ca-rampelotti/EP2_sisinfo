edf85af8f5d1ac60d93f89e157341736312120d5
ca-rampelotti <138331782+ca-rampelotti@users.noreply.github.com>
2025-11-20 15:42:02 -0300

    feat: add post model and urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(help_text='Conteúdo em HTML')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
