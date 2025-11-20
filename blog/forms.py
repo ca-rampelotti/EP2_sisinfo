7d966e7a32fbd526631a40d2192ed5f226dab220
ca-rampelotti <138331782+ca-rampelotti@users.noreply.github.com>
2025-11-20 16:01:11 -0300

    CRUD V2

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10, 'cols': 60}),
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.order_by('-created_at')
@@ -12,20 +13,27 @@ def post_detail(request, pk):
 
 def post_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title','').strip()
-        content = request.POST.get('content','')
-        post = Post.objects.create(title=title, content=content, created_at=timezone.now())
-        return redirect('blog:post_detail', pk=post.pk)
-    return render(request, 'blog/post_form.html', {'post': None})
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_form.html', {'form': form, 'post': None})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
     if request.method == 'POST':
-        post.title = request.POST.get('title', post.title)
-        post.content = request.POST.get('content', post.content)
-        post.save()
-        return redirect('blog:post_detail', pk=post.pk)
-    return render(request, 'blog/post_form.html', {'post': post})
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_form.html', {'form': form, 'post': post})
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
